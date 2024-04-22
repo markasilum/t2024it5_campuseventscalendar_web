e9abf5af11c55186631293b872f8d70442683a86
Rmdtanduyan <rmdtanduyan@addu.edu.ph>
2024-04-22 19:42:36 +0800

    LOGIN DESIGN UPDATED

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -12,7 +12,7 @@ class LoginScreen extends StatelessWidget {
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           padding: EdgeInsets.symmetric(horizontal: 130, vertical: 50),
-          color: Colors.green[100],
+          color: Color.fromARGB(255, 128, 183, 235),
           child: Center(
             child: Container(
               height: MediaQuery.of(context).size.height * .55,
@@ -26,17 +26,26 @@ class LoginScreen extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisSize: MainAxisSize.max,
                   children: [
-                    const Row(
-                      children: [
-                        Icon(Icons.calendar_month, size: 50),
-                        Text(
-                          "Campus Events",
-                          style: TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                    Center(
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 100,
+                            height: 100,
+                            child: Image(
+                              image: NetworkImage(
+                                  'https://i.imgur.com/VPg47zp.png'),
+                            ),
+                          ),
+                          Text(
+                            "Ateneo de Davao Campus Events",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
                     ),
-                     const SizedBox(
+                    const SizedBox(
                       height: 10,
                     ),
                     const Text(
@@ -45,11 +54,13 @@ class LoginScreen extends StatelessWidget {
                     const SizedBox(
                       height: 10,
                     ),
-                    const TextField(
+                    TextField(
                       decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: "name@example.com",
-                          hintStyle: TextStyle(color: Colors.grey)),
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.person,size: 24), // Adding the icon
+                        hintText: "name@example.com",
+                        hintStyle: TextStyle(color: Colors.grey),
+                      ),
                     ),
                     const SizedBox(
                       height: 10,
@@ -61,6 +72,7 @@ class LoginScreen extends StatelessWidget {
                     const TextField(
                       decoration: InputDecoration(
                           border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.key,size: 24), // Adding the icon
                           hintText: "atleast 8 characters",
                           hintStyle: TextStyle(color: Colors.grey)),
                     ),
@@ -69,30 +81,31 @@ class LoginScreen extends StatelessWidget {
                     ),
                     Column(
                       children: [
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.purple,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10))),
-                          width: 400,
-                          height: 40,
-                          child: TextButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const HomeScreen()),
-                                );
-                              },
-                              child: Text(
-                                "Login",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              )),
+                        Center(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Color(0xFF08228D),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            width: 400,
+                            height: 40,
+                            child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const HomeScreen()),
+                                  );
+                                },
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                )),
+                          ),
                         ),
-                  
                       ],
                     ),
                   ]),
