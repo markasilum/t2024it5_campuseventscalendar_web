1a9b8b65c95011ce2523a8759d0c120c6f5bf0f7
markasilum <62866585+markasilum@users.noreply.github.com>
2024-04-22 22:09:12 +0800

    ambot

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:t2024it5_campuseventscalendar_web/screens/errorPage.dart';
 import 'package:t2024it5_campuseventscalendar_web/screens/splashScreen.dart';
-import 'firebase_options.dart';
 
 import 'package:provider/provider.dart';
 import 'package:t2024it5_campuseventscalendar_web/provider/SignInProvider.dart';
@@ -10,48 +11,44 @@ import 'package:t2024it5_campuseventscalendar_web/screens/homeScreen.dart';
 import 'package:t2024it5_campuseventscalendar_web/screens/loginScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-void main()  {
-  runApp( MainApp(),
+void main() {
+  runApp(
+    MainApp(),
   );
 }
 
 class MainApp extends StatelessWidget {
-
   final Future<FirebaseApp> _initialization = Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        if(snapshot.hasError){
-          return ErrorPage();
-        }
-        if(snapshot.connectionState == ConnectionState.done){
-           return ChangeNotifierProvider(
-            create: (_)=> SignInProvider(),
-             child: MaterialApp(
-                       debugShowCheckedModeBanner: false,
-                       theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-                       ),
-                       home: LoginScreen(),
-                       routes: {
-              '/home': (context) => HomeScreen(),
-              '/login': (context) => LoginScreen()
-                       },
-                     ),
-           );
-        }
-        return SplashScreen();
-        
-      }
-    );
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return ErrorPage();
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ChangeNotifierProvider(
+              create: (_) => SignInProvider(),
+              child: MaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(
+                  colorScheme:
+                      ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                  useMaterial3: true,
+                ),
+                home: LoginScreen(),
+                routes: {
+                  '/home': (context) => HomeScreen(),
+                  '/login': (context) => LoginScreen()
+                },
+              ),
+            );
+          }
+          return SplashScreen();
+        });
   }
 }
-
-
